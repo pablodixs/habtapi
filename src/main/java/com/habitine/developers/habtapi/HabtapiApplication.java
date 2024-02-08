002a1a6381489eb2c659316e2d33ff6d7855a5e7
pablodixs <pabllosoarez@gmail.com>
2024-02-08 18:30:41 -0300

    first commit from local repository

---
+++ src/main/java/com/habitine/developers/habtapi/HabtapiApplication.java
@@ -0,0 +1,13 @@
+package com.habitine.developers.habtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabtapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitine/developers/habtapi/HabtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitine.developers.habtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
